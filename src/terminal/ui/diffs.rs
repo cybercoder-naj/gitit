9594d62b4166434c284e1e0a4d31e02b4cfe6100
Nishant Aanjaney Jalan <nishantaanjaney.jalan@news.co.uk>
2023-08-12 11:00:18 +0100

    Show formatted git diff for every file

--- src/terminal/ui/diffs.rs
+++ src/terminal/ui/diffs.rs
@@ -2,7 +2,8 @@ use ratatui::{
     backend::Backend,
     Frame,
     layout::Rect, 
-    widgets::{Block, Borders, Paragraph, Padding}
+    widgets::{Block, Borders, Paragraph, Padding},
+    text::{Line, Span}, style::{Style, Modifier, Color}
 };
 
 use crate::controller::{state::State, self};
@@ -21,6 +22,76 @@ pub fn render<B: Backend>(frame: &mut Frame<B>, area: Rect, state: &State) {
 pub fn generate_git_paragaph<'a>(block: Block<'a>, state: &'a State) -> Paragraph<'a> {
     let m_file = state.get_current_file();
 
-    Paragraph::new(controller::get_diff_string(m_file))
+    let binding = controller::get_diff_string(m_file);
+    let diff = binding.lines();
+    let mut text: Vec<Line> = vec![];
+
+    for line in diff.zip(0..) {
+        if line.1 < 4 {
+            text.push(
+                Line::from(
+                    Span::styled(
+                        String::from(line.0),
+                        Style::default().add_modifier(Modifier::BOLD)
+                    )
+                )
+            );   
+            continue;
+        }
+
+        if line.0.starts_with("@@") {
+            let end_ref = line.0.rfind("@@").expect("Git diff returned wrong formating"); 
+            text.push(
+                Line::from(
+                    vec![
+                        Span::styled(
+                            String::from(&line.0[..(end_ref + 2)]),
+                            Style::default().fg(Color::LightBlue)
+                        ),
+                        Span::raw(
+                            String::from(&line.0[(end_ref + 2)..])
+                        )
+                    ]
+                )
+            );   
+            continue;
+        }
+
+        if line.0.starts_with("+") {
+            text.push(
+                Line::from(
+                    Span::styled(
+                        String::from(line.0),
+                        Style::default().fg(Color::Green)
+                    )
+                )
+            );   
+            continue;
+        }
+
+        if line.0.starts_with("-") {
+            text.push(
+                Line::from(
+                    Span::styled(
+                        String::from(line.0),
+                        Style::default().fg(Color::Red)
+                    )
+                )
+            );   
+            continue;
+        }
+
+        text.push(
+            Line::from(
+                Span::styled(
+                    String::from(line.0),
+                    Style::default()
+                )
+            )
+        );   
+    }
+
+
+    Paragraph::new(text)
         .block(block)
 }
